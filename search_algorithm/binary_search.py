892b4366d546dfd24b051159c144ce2460c13905
andredarochasouza <andredarochasouza.12345@gmail.com>
2022-05-17 15:56:13 -0300

    add binary search

--- search_algorithm/binary_search.py
+++ search_algorithm/binary_search.py
@@ -11,25 +11,47 @@ Algoritmo Binary Search
 import time
 import random
 
-arr = []
-for i in range(5000):
-    arr.append(i * random.randint(1, 10))
+# Insertion Sort
+def inserction_sort(arr):
+    for i in range(5000):
+        arr.append(i * random.randint(1, 10))
 
-for i in range(len(arr)):
-    j = i
-    while j > 0 and arr[j] < arr[j-1]:
-        temp = arr[j]
-        arr[j] = arr[j - 1]
-        arr[j - 1] = temp
-        j -= 1
+    for i in range(len(arr)):
+        j = i
+        while j > 0 and arr[j] < arr[j-1]:
+            temp = arr[j]
+            arr[j] = arr[j - 1]
+            arr[j - 1] = temp
+            j -= 1
+    return arr
+
+# Search Binary
+def binary_search(arr, value):
+    ind_menor = 0
+    ind_maior = len(arr) - 1
+
+    while ind_menor <= ind_maior:
+        ind_central = round((ind_menor + ind_maior) / 2)
+        temp = arr[ind_central]
+
+        if temp == value:
+            return ind_central
+        if temp > value:
+            ind_maior = ind_central - 1
+        else:
+            ind_menor = ind_central + 1
 
 start_time = time.time()
 
-# Em desenvolvimento
+num_buscas = 10
+arr = []
+inserction_sort(arr)
 
+for i in range(10):
+    print(binary_search(arr, random.randint(1, len(arr))))
+    
 
 finish_time = time.time()
 finish_time = finish_time - start_time
 
-print(arr)
 print("Tempo de execução: %.5f segundos" % finish_time)
